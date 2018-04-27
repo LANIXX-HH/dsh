aa48729e20b2aa015a68d7cfee109e40b2024760
Anatoli Lichii <anatoli@lanixx.com>
2018-04-27 22:43:23 +0200

    init commit: docker setup, entrypoint, docker-compose.yml

---
+++ data/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+test -f /home/user/.lanixx && ( echo "..."
+
+  _UID=$(ls -ldn /home/user/.lanixx | awk '{print $3}')
+  _GID=$(ls -ldn /home/user/.lanixx | awk '{print $4}')
+
+  if (test $_UID -gt 33); then 
+    echo change user uid to $_UID ...
+    usermod -u $_UID user &
+  else
+    echo change user uid to 1000 ...
+    usermod -u 1000 user &
+  fi
+  sleep 1
+
+  if (test $_GID -gt 33 || test $_GID -lt 33 ); then
+    echo change gid $_GID ...
+    set -x
+    groupmod -o -g $_GID user &
+    set +x
+  else
+    echo change gid 1000 ...
+    groupmod -o -g 1000 user &
+  fi
+  sleep 1
+
+)
+
+chmod 666 /var/run/docker.sock
+
+echo "su -c \"test -f ~/.bash_it/install.sh || ( git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it && ~/.bash_it/install.sh ) \" user" > ~/.bashrc
+
+while sleep 3600; do :; done
